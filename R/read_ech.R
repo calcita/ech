58f57d36e7fd6d066de19bdbd92c6826fe5abd65
calcita <calcita@gmx.li>
2019-11-29 16:56:08 -0300

    added employment_rate function

---
+++ R/read_ech.R
@@ -0,0 +1,9 @@
+#' A data Function
+#'
+#' This function allows you to read ECH's microdata.
+#' @param year Defaults 2018.
+#' @keywords data
+#' @export
+#' @import dplyr
+#' @examples
+#' read_ech()
