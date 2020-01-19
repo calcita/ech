9aea910a1479f0b50300ea2b7c76eee8c95125e8
gmathieux <calcita@gmx.li>
2020-01-19 15:41:57 -0300

    starting gini function

---
+++ R/gini.R
@@ -0,0 +1,30 @@
+#' gini
+#'
+#' @param data
+#' @param weights
+#' @param domain
+#'
+#' @return
+#' @export
+#'
+#' @examples
+gini <- function(data = df,
+                 weights = pesoano,
+                 domain = NULL) {
+
+  #data %<>% filter (nper == 1)
+  # ipc montevideo
+
+  data <- income_constant_prices(data = data, base.month = 1, base.year = 2005)
+  data %<>% mutate(ypc_deflate_gini = ht11_svl_per_capita_deflate) %>%
+    select(-ht11_per_capita:-ht11_per_capita_deflate) # ver como no sobreescribir
+
+  # ipc interior
+
+  if (domain = NULL) {
+    indice <- laeken::gini(inc = "ypcsvl_deflate_gini", weights = "pesoano", data = data)
+  } else {
+    indice <- laeken::gini("ypcsvl_deflate_gini", weights = "pesoano", data = data, breakdown = domain)
+  }
+
+}
