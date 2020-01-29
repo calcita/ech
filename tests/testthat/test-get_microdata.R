f5bbc2396008c5fc4a7acab344d301577f1e3705
Richard Detomasi <richard.detomasi@gmail.com>
2020-01-29 10:34:44 -0300

    again and again

--- tests/testthat/test-get_microdata.R
+++ tests/testthat/test-get_microdata.R
@@ -1,7 +1,8 @@
 context("Testing get_microdata")
-
+library(haven)
 test_that("download and read data", {
-  aaa <- get_microdata(year = "2016", folder = tempdir(), toR = FALSE)
+  get_microdata(year = "2016", folder = tempdir(), toR = FALSE)
+  aaa <- haven::read_sav(paste0(tempdir(),"\\HyP_2016_Terceros.sav"))
   testthat::expect_is(aaa, "data.frame")
   testthat::expect_equal(nrow(aaa), 118591L)
 })
