87125bbce900becbf30f96aa148507409dc7ef40
Richard Detomasi <richard.detomasi@gmail.com>
2020-01-29 07:54:38 -0300

    travi... eso

--- tests/testthat/test-get_microdata.R
+++ tests/testthat/test-get_microdata.R
@@ -1,7 +1,7 @@
 context("Testing get_microdata")
 
 test_that("download and read data", {
-  aaa <- get_microdata(year = "2016", folder = tempdir(), toR = FALSE)
+  aaa <- suppressWarnings(get_microdata(year = "2016", folder = tempdir(), toR = FALSE))
   testthat::expect_is(aaa, "data.frame")
   testthat::expect_equal(nrow(aaa), 118591L)
 })
