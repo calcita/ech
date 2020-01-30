427e68384e4194c093a302dac7c76cbb1dfede7f
Richard Detomasi <richard.detomasi@gmail.com>
2020-01-30 14:40:28 -0300

    a por la negativa

--- tests/testthat/test-get_microdata.R
+++ tests/testthat/test-get_microdata.R
@@ -1,7 +1,7 @@
 context("Testing get_microdata")
 library(haven)
 test_that("download and read data", {
-  testthat::expect_warning(get_microdata(year = "2016", folder = tempdir(), toR = FALSE))
+  testthat::expect_error(get_microdata(year = "2016", folder = tempdir(), toR = FALSE))
   # aaa <- haven::read_sav(paste0(tempdir(),"\\HyP_2016_Terceros.sav"))
   # testthat::expect_is(aaa, "data.frame")
   # testthat::expect_equal(nrow(aaa), 118591L)
