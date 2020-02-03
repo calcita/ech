ac01d6524f8e85786e0fc0313f78c87ba1f431fd
Richard Detomasi <richard.detomasi@gmail.com>
2020-02-03 10:11:31 -0300

    prices y logo

--- R/income_constant_prices.R
+++ R/income_constant_prices.R
@@ -9,10 +9,10 @@
 #' @param ysvl ysvl
 #' @param ht13 ht13
 #' @param ht19 ht19
-#' @importFrom dplyr mutate left_join enquo
+#' @importFrom dplyr mutate left_join
 #' @importFrom magrittr %<>% %>%
 #' @importFrom haven zap_labels
-#' @importFrom rlang sym quo_name
+#' @importFrom rlang .data
 #' @export
 #' @examples
 #' \donttest{
@@ -26,26 +26,21 @@ income_constant_prices <- function(data = ech::toy_ech_2017_income,
                                    ysvl = "YSVL",
                                    ht13 = "ht13",
                                    ht19 = "ht19"){
-  mes = quo_name(enquo(mes))
-  ht11 = quo_name(enquo(ht11))
-  ysvl = quo_name(enquo(ysvl))
-  ht13 = quo_name(enquo(ht13))
-  ht19 = quo_name(enquo(ht19))
+
   #load("data/toy_ech_2017_income.rda")
   deflate <- deflate(base.month = base.month,
                      base.year = base.year)
   # Asigna deflactor
-  data <- data %>% dplyr::mutate(aux = as.integer(haven::zap_labels(!!sym(mes))))
+  data <- data %>% dplyr::mutate(aux = as.integer(haven::zap_labels(.data[[mes]])))
 
   data <- dplyr::left_join(data, deflate, by = c("aux" = "mes"))
 
   # Ingresos deflactados
-  data %<>% dplyr::mutate(ht11_per_capita = !!sym(ht11) / !!sym(ht19),
-                 ht11_deflate = !!sym(ht11) * deflate,
-                 ht13_deflate = !!sym(ht13) * deflate,
-                 ht11_svl_def = !!sym(ysvl) * deflate,
-                 ht11_svl_per_capita_deflate = !!sym(ysvl) / !!sym(ht19) * deflate,
-                 ht11_per_capita_deflate = !!sym(ht11) / !!sym(ht19) * deflate # Ingresos promedio per cápita a precios constantes de month.base year.base
+  data %<>% dplyr::mutate(ht11_per_capita = .data[[ht11]] / .data[[ht19]],
+                 ht11_deflate = .data[[ht11]] * deflate,
+                 ht13_deflate = .data[[ht13]] * deflate,
+                 ht11_svl_def = .data[[ysvl]] * deflate,
+                 ht11_svl_per_capita_deflate = .data[[ysvl]] / .data[[ht19]] * deflate,
+                 ht11_per_capita_deflate = .data[[ht11]] / .data[[ht19]] * deflate # Ingresos promedio per cápita a precios constantes de month.base year.base
   )
-
 }
--- man/figures/ech_hex.R
+++ man/figures/ech_hex.R
@@ -2,27 +2,14 @@ library(hexSticker) # https://github.com/GuangchuangYu/hexSticker
 
 # add special text font
 library(sysfonts)
-font_add_google(name = "Roboto", family = "Roboto")
+font_add_google(name = "BOLD", family = "BEBAS NEUE")
 
 library(extrafont)
 font_import()
 loadfonts(device = "win")
 
 ### .png
-sticker("https://cdn.imgbin.com/17/13/2/imgbin-capybara-rodent-rat-wikia-rat-0SuAbJNicCjwzZ0Ci7DtL7yQh.jpg", package = "ech",
-        s_x = 1.03, s_y = 1, s_width = 1.5, s_height = 1.5, # ggplot image size and position
-        h_fill = "#4dc0d1", h_color = "#4dc0d1", # hexagon
-        url = "https://github.com/holatam/ech", u_color = "black", u_size = 6,
-        spotlight = T, l_height = 17,
-        filename = "./man/figures/ech_logo.png", dpi = 400)  # output name and resolution
-
-
-### .svg
-sticker("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTK-_QKgH49LY-a9Pj3yZdn8SewwMiJP65MmGqiD5L-whxXLlsl", package = "ech",
-        s_x = 1.03, s_y = 1, s_width = 1.5, s_height = 1.5, # ggplot image size and position
-        h_fill = "#4dc0d1", h_color = "#4dc0d1", # hexagon
-        url = "https://github.com/holatam/ech", u_color = "black",
-        spotlight = T, l_height = 17,
-        filename = "./man/figures/ech_logo.svg")  # output name and resolution
-
-
+sticker("U:/carpincha1.png",
+        package = "ech", p_size = 20, s_x = 1, s_y = .75, s_width = .6, s_height = 0.7, h_color = "gray",
+        h_fill = "#74a9cf", url = "https://github.com/holatam/ech", u_color = "black",
+        u_size = 3, filename = "./man/figures/ech_logo.svg")
