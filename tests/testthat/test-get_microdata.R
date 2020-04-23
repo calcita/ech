9cbe96bfdadb1b35c830e909932cfcad8630386f
Richard Detomasi <richard.detomasi@gmail.com>
2020-04-23 17:22:24 -0300

    por algun punto mas de codcov

--- tests/testthat/test-get_microdata.R
+++ tests/testthat/test-get_microdata.R
@@ -2,6 +2,8 @@ context("Testing get_microdata")
 library(haven)
 test_that("download and read data", {
   testthat::expect_error(get_microdata(year = "2016", folder = tempdir(), toR = FALSE))
+  testthat::expect_error(get_microdata(folder = tempdir(), toR = FALSE))
+  testthat::expect_error(get_microdata(folder = 1, toR = FALSE))
   # aaa <- haven::read_sav(paste0(tempdir(),"\\HyP_2016_Terceros.sav"))
   # testthat::expect_is(aaa, "data.frame")
   # testthat::expect_equal(nrow(aaa), 118591L)
