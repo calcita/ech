565335c11e40d8aebb5dd99723404d438ac26ee4
gmathieux <calcita@gmx.li>
2020-04-23 11:09:07 -0300

    quité argumentos, agregué controles.

--- R/read_microdata.R
+++ R/read_microdata.R
@@ -1,34 +1,35 @@
 #' Read ECH from a local folder
-#' @param folder Folder where are the files or be download
-#' @param filename File name
-#' @param format allows read data in rds, rdata, sav and dta
+#' @param path Folder where are the files or be download
 #' @importFrom fs path
 #' @importFrom haven read_sav read_dta
-#' @return unrar files from INE web and the respective data frame in tibble format
+#' @return an object called df
 #' @export
 #' @examples
-#' read_microdata(folder = "/home/calcita/Escritorio/pruebaech/", filename = "HyP_2017_Terceros.sav")
+#' read_microdata(folder = "/home/usuarie/Escritorio/HyP_2017_Terceros.sav")
 
-read_microdata <- function(folder = NULL, filename = NULL, format = "rds"){
+read_microdata <- function(path = NULL){
 
-  # stopifnot(format %in% c("rds", "rdata", "sav", "dta"))
-  # #stopifnot(is.character(filename))
-  #
-  # if (!is.null(folder)) {
-  #   archivo <- fs::path(folder, paste0(filename, ".",format))
-  # } else {
-  #   archivo <- ech::toy_ech_2017_income
-  # }
-  #
-  # # read spss file ---
-  # if(format == "sav"){
-  #   df <- haven::read_sav(archivo)
-  # } else if(format == "dta"){
-  #   df <- haven::read_dta(archivo)
-  # } else if (format == "rds"){
-  #   df <- readRDS(archivo)
-  # } else{
-  #   load(archivo)
-  # }
+  #stopifnot(is.character(filename))
+
+  if (!is.null(path)) {
+    format = tolower(fs::path_ext(path))
+    if (!format %in% c("sav", "dta", "rds", "rdata"))
+      stop(glue::glue("No es posible abrir un archivo con extensión {format}"))
+  } else {
+    stop(glue::glue("No es posible abrir {path}"))
+  }
+
+  # read spss file ---
+  if(format == "sav"){
+    df <- haven::read_sav(path)
+      haven::zap_labels() %>%  haven::zap_formats() %>%  haven::zap_label()
+  } else if(format == "dta"){
+    df <- haven::read_dta(path)
+      haven::zap_labels() %>%  haven::zap_formats() %>%  haven::zap_label()
+  } else if (format == "rds"){
+    df <- readRDS(path)
+  } else{
+    load(archivo)
+  }
 
 }
