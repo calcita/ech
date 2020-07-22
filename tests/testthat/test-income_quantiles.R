fac86abb81247df94fedc3018cf88e52a42ad7e8
richdeto <richard.detomasi@gmail.com>
2020-07-22 13:22:55 -0300

    v0.0.1.13

--- tests/testthat/test-income_quantiles.R
+++ tests/testthat/test-income_quantiles.R
@@ -1,6 +1,6 @@
 test_that("income_quantiles works", {
-  a <- income_constant_prices()
-  b <- income_quantiles(data = a)
+  a <- income_constant_prices(data = ech::toy_ech_2018)
+  b <- income_quantiles(a)
   expect_equal(ncol(b), ncol(a) + 1)
   c <- income_quantiles(data = a, quantile = 10)
   expect_equal(ncol(c), ncol(a) + 1)
