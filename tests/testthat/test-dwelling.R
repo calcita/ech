d60f4ccfe19b396e72449a217292f6362c409b9c
richdeto <richard.detomasi@gmail.com>
2020-07-24 16:07:07 -0300

    v0.0.1.14

--- tests/testthat/test-dwelling.R
+++ tests/testthat/test-dwelling.R
@@ -2,7 +2,7 @@ test_that("dwelling works", {
   toy_ech_2018 <- income_constant_prices(data = ech::toy_ech_2018)
   toy_ech_2018 <- income_quantiles(data = toy_ech_2018)
   toy_ech_2018 <- housing_deprivation(data = toy_ech_2018)
-  expect_equal(ncol(toy_ech_2018), 600)
+  expect_equal(ncol(toy_ech_2018), 598)
   })
 
 test_that("housing works", {
