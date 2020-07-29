c220df9cf8beb281a082679a1a28a19714a8407b
richdeto <richard.detomasi@gmail.com>
2020-07-29 09:23:35 -0300

    v0.0.1.14

--- tests/testthat/test-get_microdata.R
+++ tests/testthat/test-get_microdata.R
@@ -10,5 +10,7 @@ test_that("download and read data", {
   # aaa <- haven::read_sav(paste0(tempdir(),"\\HyP_2016_Terceros.sav"))
   # testthat::expect_is(aaa, "data.frame")
   # testthat::expect_equal(nrow(aaa), 118591L)
+  testthat::expect_error(get_marco(year = "2016", folder = tempdir(), toR = TRUE))
+  testthat::expect_error(get_marco(year = "2016", folder = tempdir(), toR = FALSE))
 })
 
